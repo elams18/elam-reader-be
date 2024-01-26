7d5c5bc446c00b355cb65a18eba8cb9379187be4
elamathis18 <elamathi@bewelldigital.com>
2024-01-26 16:59:54 +0530

    initial spring boot setup

---
+++ src/main/java/com/be/elamreader/ElamReaderApplication.java
@@ -0,0 +1,13 @@
+package com.be.elamreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElamReaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElamReaderApplication.class, args);
+    }
+
+}
