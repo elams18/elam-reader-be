e9f891dbe748b2bf2ef480e2fe04f61a2369f666
elams18 <elamathis18@gmail.com>
2024-02-02 21:34:35 +0530

    add test properties for test cases and update dockerfile for java 21

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM --platform=linux/amd64 eclipse-temurin:17-jdk-alpine
+FROM --platform=linux/amd64 openjdk:21-ea-jdk
 VOLUME /tmp
 COPY target/*.jar app.jar
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/test/java/com/be/elamreader/ElamReaderApplicationTests.java
+++ src/test/java/com/be/elamreader/ElamReaderApplicationTests.java
@@ -3,7 +3,7 @@ package com.be.elamreader;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.config.name=application-test")
 class ElamReaderApplicationTests {
 
     @Test
